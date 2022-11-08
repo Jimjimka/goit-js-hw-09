510e7bbd8263f20b374165d89ca39b55fe71623e
Jimjimka <106761382+Jimjimka@users.noreply.github.com>
2022-11-08 15:27:17 +0000

    Deploying to gh-pages from @ Jimjimka/goit-js-hw-09@8fbcdca9bf699c9d40963ae5636ff58ce7f78db9 🚀

---
+++ 01-color-switcher.2e61627b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");var o=null;e.addEventListener("click",(function(){o=setInterval((function(){t.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.2e61627b.js.map
